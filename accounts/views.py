4636d2134eb47925ccbb1ff91cf7a8b47dce6e37
shafiq <ssekittoshafiq@gmail.com>
2024-08-13 12:09:41 +0300

    noc

--- accounts/views.py
+++ accounts/views.py
@@ -46,14 +46,6 @@ def offShore(request):
             new_athlete = cform.save(commit=False)
 
             # Handle the cropped image
-            cropped_image_data = request.POST.get("croppedImage")
-            if cropped_image_data:
-                format, imgstr = cropped_image_data.split(";base64,")
-                ext = format.split("/")[-1]
-                image_data = base64.b64decode(imgstr)
-                new_athlete.photo = ContentFile(
-                    image_data, name=f"athlete_{new_athlete.id}.{ext}"
-                )
 
             new_athlete.save()
             messages.success(request, "Form submitted successfully.")
