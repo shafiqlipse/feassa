9ac51528802ae03e73dcef64c33893187c5acd58
shafiq <ssekittoshafiq@gmail.com>
2026-08-22 12:47:12 +0300

    omoding

--- comittee/views.py
+++ comittee/views.py
@@ -455,7 +455,7 @@ def all_OfficiatingOfficialsAccreditation(request):
     match_officials = OfficiatingOfficials.objects.all()
 
     # Apply the filter
-    match_officials_filter = mediaFilter(request.GET, queryset=match_officials)
+    match_officials_filter = match_official_filter(request.GET, queryset=match_officials)
     filtered_match_officials = match_officials_filter.qs
 
     if request.method == "POST":
